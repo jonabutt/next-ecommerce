d73ec753b28b1efb7aa79879773b72dcb5b81395
jonabutt <jonathanbuttigieg1@gmail.com>
2022-01-27 22:34:01 +0100

    feat: added patch and delete methods

--- utils/fetchAPI.ts
+++ utils/fetchAPI.ts
@@ -16,6 +16,7 @@ export const postData = async (url:string,data:any,token:string) => {
     const res = await fetch(`${baseUrl}/api/${url}`,{
         method: 'POST',
         headers: {
+            'Content-Type': 'application/json',
             'Authorization': token
         },
         body: JSON.stringify(data)
@@ -29,9 +30,37 @@ export const putData = async (url:string,data:any,token:string) => {
     const res = await fetch(`${baseUrl}/api/${url}`,{
         method: 'PUT',
         headers: {
+            'Content-Type': 'application/json',
             'Authorization': token
         },
         body: JSON.stringify(data)
+    });
+
+    const resultData = await res.json();
+    return resultData;
+}
+
+export const patchData = async (url:string,data:any,token:string) => {
+    const res = await fetch(`${baseUrl}/api/${url}`,{
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        },
+        body: JSON.stringify(data)
+    });
+
+    const resultData = await res.json();
+    return resultData;
+}
+
+export const deleteData = async (url:string,token:string) => {
+    const res = await fetch(`${baseUrl}/api/${url}`,{
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        }
     });
 
     const resultData = await res.json();
