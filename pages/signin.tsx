e0440004592c5cfee4fb4286ab6abe58d72a93a7
jonabutt <jonathanbuttigieg1@gmail.com>
2022-01-30 17:37:14 +0100

    feat: redirect user to home page on signin

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, FormEvent, useState , useContext} from 'react'
-import { LockOutlined, Visibility, VisibilityOff } from '@mui/icons-material'
+import { ChangeEvent, FormEvent, useState , useContext, useEffect} from 'react'
+import { LockOutlined, Router, Visibility, VisibilityOff } from '@mui/icons-material'
 import { Avatar, Button, FormControl, Grid, IconButton, InputAdornment, InputLabel, Link as MUILink, OutlinedInput, Paper, TextField, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import Head from 'next/head'
@@ -10,6 +10,7 @@ import {postData} from '../utils/fetchAPI'
 import ActionKind from '../store/Actions'
 import Cookie from 'js-cookie'
 import { PayloadAuth } from '../interfaces'
+import { useRouter } from 'next/router'
 
 interface LoginData {
   username: string;
@@ -22,17 +23,24 @@ const Signin: NextPage = () => {
   const [loginData, setLoginData] = useState<LoginData>(initialState);
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const { state , dispatch } = useContext(DataContext);
-
+  const {auth}= state;
   const {username,password} = loginData;
 
 
   const paperStyle = { padding:20, display: "flex", flexDirection:"column", alignItems: "center"};
   const avatarStyle = { backgroundColor : "#1bbd7e"};
 
+  const router = useRouter();
+
+  useEffect(()=>{
+    if(auth!==null){
+      router.push('/');
+    }
+  },[auth]);
+
   const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     setLoginData({...loginData,[e.currentTarget.name]:e.currentTarget.value})
   }
-
   const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
   };
