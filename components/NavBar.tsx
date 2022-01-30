5b49b0c575554b905d597569a542a4e1a04c6581
jonabutt <jonathanbuttigieg1@gmail.com>
2022-01-30 17:37:03 +0100

    feat: added profile menu on while signed in

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,34 +1,57 @@
-import React from 'react'
-import { AppBar, Avatar, Box, Button, IconButton,Link as MUILink,  Menu,  MenuItem,  Toolbar, Tooltip, Typography } from "@mui/material"
+import React,{useContext} from 'react'
+import { AppBar, Avatar, Box, Divider, IconButton,Link as MUILink,  ListItemIcon,  Menu,  MenuItem,  Toolbar, Tooltip, Typography } from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PersonIcon from '@mui/icons-material/Person';
 import Link from "next/link";
 import { useRouter } from 'next/router';
-
-
+import { DataContext } from '../store/GlobalState';
+import { Logout } from '@mui/icons-material';
+import toast from 'react-hot-toast';
+import ActionKind from '../store/Actions';
+import Cookie from 'js-cookie';
 
 const NavBar = () => {
     const [anchorElNav, setAnchorElNav] = React.useState<HTMLElement | null>(null);
     const [anchorElUser, setAnchorElUser] = React.useState<HTMLElement | null>(null);
+   
+    const [anchorElProfile, setAnchorElProfile] = React.useState<HTMLElement | null>(null);
+    const profileIsOpen = Boolean(anchorElProfile);
+
+    const router = useRouter();
+    const {state,dispatch} = useContext(DataContext);
+    const {auth} = state;
 
     const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElNav(event.currentTarget);
     };
-
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElUser(event.currentTarget);
     };
-
     const handleCloseNavMenu = () => {
         setAnchorElNav(null);
     };
-
     const handleCloseUserMenu = () => {
         setAnchorElUser(null);
     };
+    const handleProfileClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElProfile(event.currentTarget);
+    };
+    const handleProfileClose = () => {
+        setAnchorElProfile(null);
+    };
 
-    const router = useRouter();
+    const handleLogout = () => {
+        // remove cookie and local storage
+        Cookie.remove('refreshToken',{path:'api/auth/accessToken'})
+        localStorage.removeItem('firstLogin');
+        // reset the userdetails in the Global store
+        dispatch({ type: ActionKind.AUTH, payload: null });
+        // show toast
+        toast.success("Logged out!");
+        // redirect to home page
+        return router.push('/');
+    };
 
     const isActive = (r:String) => {
         if(r===router.pathname){
@@ -36,7 +59,73 @@ const NavBar = () => {
         }
         return false;
     }
+    const loggedRouter = () => {
+        return(
+            <>
+                <Tooltip title="Account settings">
+                    <IconButton
+                        onClick={handleProfileClick}
+                        size="small"
+                        sx={{ ml: 2 , padding: 0}}
+                        aria-controls={profileIsOpen ? 'account-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={profileIsOpen ? 'true' : undefined}
+                    >
+                        <Avatar sx={{ width: 32, height: 32 }}>{auth?.user.name[0].toUpperCase()}</Avatar>
+                    </IconButton>
+                </Tooltip>
+                <Menu
+                    anchorEl={anchorElProfile}
+                    id="account-menu"
+                    open={profileIsOpen}
+                    onClose={handleProfileClose}
+                    onClick={handleProfileClose}
+                    PaperProps={{
+                        elevation: 0,
+                        sx: {
+                                overflow: 'visible',
+                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                mt: 1.5,
+                                '& .MuiAvatar-root': {
+                                width: 32,
+                                height: 32,
+                                ml: -0.5,
+                                mr: 1,
+                            },
+                            '&:before': {
+                                content: '""',
+                                display: 'block',
+                                position: 'absolute',
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                bgcolor: 'background.paper',
+                                transform: 'translateY(-50%) rotate(45deg)',
+                                zIndex: 0,
+                            },
+                        }
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                    <MenuItem>
+                        <Avatar /> Profile
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem onClick={handleLogout}>
+                        <ListItemIcon>
+                            <Logout fontSize="small" />
+                        </ListItemIcon>
+                        Logout
+                    </MenuItem>
+                </Menu>
+            </>
+            
 
+
+        )
+    }
     return (
         <AppBar position="static">
             <Toolbar variant="dense">
@@ -123,54 +212,25 @@ const NavBar = () => {
                             </MenuItem>
                         </MUILink>
                     </Link>
-                    <Link href="/signin" passHref>
-                        <MUILink color="inherit" underline="none">
-                            <MenuItem sx={{ "&&.Mui-selected": {
-                                    backgroundColor: "#001F5E"
-                                }}} selected={isActive("/signin")} justify-self="flex-end" onClick={handleCloseNavMenu}>
-                                <PersonIcon/>
-                                <Typography textAlign="center">Sign in</Typography>
-                            </MenuItem>
-                        </MUILink>
-                    </Link>
+                    
+                    {
+                        (auth === null)   ?
+                        <Link href="/signin" passHref>
+                            <MUILink color="inherit" underline="none">
+                                <MenuItem sx={{ "&&.Mui-selected": {
+                                        backgroundColor: "#001F5E"
+                                    }}} selected={isActive("/signin")} justify-self="flex-end" onClick={handleCloseNavMenu}>
+                                    <PersonIcon/>
+                                    <Typography textAlign="center">Sign in</Typography>
+                                </MenuItem>
+                            </MUILink>
+                        </Link>                    
+                        :
+                        loggedRouter()
+                    }
+                    
                 </Box>
-                {/* <Box sx={{ flexGrow: 0 }}>
-                    <Tooltip title="Open settings">
-                        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                            <Avatar alt="Remy Sharp"  />
-                        </IconButton>
-                    </Tooltip>
-                    <Menu
-                        sx={{ mt: '45px' }}
-                        id="menu-appbar"
-                        anchorEl={anchorElUser}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        open={Boolean(anchorElUser)}
-                        onClose={handleCloseUserMenu}
-                    >
-                        <MenuItem  onClick={handleCloseNavMenu}>
-                            <Typography textAlign="center">Profile</Typography>
-                        </MenuItem>
-                        <MenuItem  onClick={handleCloseNavMenu}>
-                            <Typography textAlign="center">Logout</Typography>
-                        </MenuItem>
-                    </Menu>
-                </Box> */}
-                {/* <Link href="/">
-                    <MUILink color="inherit">JB E-Commerce</MUILink>
-                </Link>
-
-                <Link href="/cart">
-                    <MUILink color="inherit">Cart</MUILink>
-                </Link> */}
+                
             </Toolbar>
         </AppBar>
     )
