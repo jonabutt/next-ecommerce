f90bc0cb8b5e980538786b704b6e84e6cc5f8856
jonabutt <jonathanbuttigieg1@gmail.com>
2022-02-17 20:48:55 +0100

    feat: added modal to confirm delete cart item

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -1,13 +1,21 @@
-import { useContext } from 'react';
-import { Box, Button, Typography,Link as MUILink, IconButton} from "@mui/material";
+import { useContext, useState } from 'react';
+import { Box, Button, Typography,Link as MUILink, IconButton, Modal} from "@mui/material";
 import Image from "next/image";
 import { CartItemType } from "../interfaces";
 import { DataContext } from "../store/GlobalState";
 import { increaseQuantityCartItem, decreaseQuantityCartItem, removeCartItem } from "../store/Actions";
 import Link from 'next/link';
-import { Delete } from '@mui/icons-material';
+import { Close, Delete } from '@mui/icons-material';
 
-
+const modalStyle = {
+   position: 'absolute',
+   top: '50%',
+   left: '50%',
+   transform: 'translate(-50%,-50%)',
+   width: '400px',
+   background: '#ffffff',
+   padding: '10px 15px'
+};
 type Props = {
     cartItem: CartItemType
 };
@@ -15,6 +23,7 @@ type Props = {
 const CartItem = ({cartItem}:Props) => {
     const { state, dispatch } = useContext(DataContext);
     const { cart } = state;
+    const [modalIsOpen,setModalIsOpen] = useState<boolean>(false);
 
     return <Box sx={{ 
         display: 'flex', 
@@ -61,15 +70,45 @@ const CartItem = ({cartItem}:Props) => {
             </Typography>
             <Button variant="outlined" onClick={()=>dispatch(increaseQuantityCartItem(cart,cartItem.productId))}>+</Button>
         </Box>
+      
+        {/*  */}
+      
         <Box sx={{ width: '20px', display: 'flex', margin: 0}}>
             <IconButton
-                onClick={()=>dispatch(removeCartItem(cart,cartItem.productId))}
+                onClick={()=>setModalIsOpen(true)}
                 size="small"
                 sx={{ padding: 0, margin: 0}}
             >
                 <Delete sx={{ width: 32 }}/>
             </IconButton>
         </Box>
+        <Modal
+    
+            open={modalIsOpen}
+            onClose={()=>setModalIsOpen(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={modalStyle}>
+                <IconButton
+                    onClick={()=>setModalIsOpen(false)}
+                    size="small"
+                    sx={{ padding: 0, margin: 0,position:'absolute', top:5, right:5,"&:hover": { color: "#d32f2f" }}}
+                >
+                    <Close sx={{ width: 20 }}/>
+                </IconButton>
+                <Typography sx={{textAlign: 'center'}} id="modal-modal-title" variant="h5" component="h2">
+                    Remove
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    Are you sure you want to delete {cartItem.name} from the cart?
+                </Typography>
+                <Box sx={{display: 'flex',justifyContent:'flex-end',}}>
+                    <Button variant='contained' onClick={()=>setModalIsOpen(false)}>No</Button>
+                    <Button sx={{marginLeft:'5px'}} onClick={()=>dispatch(removeCartItem(cart,cartItem.productId))}>Yes</Button>
+                </Box>
+            </Box>
+      </Modal>
     </Box>
 }
 
