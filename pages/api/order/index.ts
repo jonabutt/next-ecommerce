9dcbacad7d167309c886f774984952494c5e1673
jonabutt <jonathanbuttigieg1@gmail.com>
2022-03-02 22:04:28 +0100

    feat: insert order and details into database

--- pages/api/order/index.ts
+++ pages/api/order/index.ts
@@ -1,9 +1,19 @@
+import { Order, OrderDetail, PrismaClient } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import auth, { ErrorResponseData, SuccessResponseData } from '../../../middleware/auth';
+import { CartItemType } from '../../../interfaces';
+import auth, { MiddlewareErrorResponseData, MiddlewareSuccessResponseData } from '../../../middleware/auth';
+
+const prisma = new PrismaClient();
+
+type SuccessResponseData = {
+    success: boolean,
+    msg: string,
+    orderId: string
+}
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<ErrorResponseData | SuccessResponseData>
+    res: NextApiResponse<MiddlewareErrorResponseData | MiddlewareSuccessResponseData | SuccessResponseData>
 ) {
 
     switch (req.method) {
@@ -15,19 +25,54 @@ export default async function handler(
 
 const createOrder = async (
     req: NextApiRequest,
-    res: NextApiResponse<ErrorResponseData | SuccessResponseData>) => {
+    res: NextApiResponse<MiddlewareErrorResponseData | MiddlewareSuccessResponseData | SuccessResponseData>) => {
     try {
-        const result = await auth(req, res)
-        const { cart, total } = req.body
-        console.log(req.body);
-        console.log(cart);
+        // check if the user is authorized
+        const result = await auth(req, res) as MiddlewareSuccessResponseData;
+        // get cart and total from request
+        const { cart, total }: { cart: CartItemType[]; total: Number } = req.body
+        // update product sold counter in database     
+        const updateProductsPromises = cart.map(c =>
+            prisma.product.update({
+                where: {
+                    id: c.productId
+                },
+                data: {
+                    soldCount: {
+                        increment: c.quantity
+                    }
+                }
+            })
+        );
+        // create order and order details in database
+        const createOrderPromise = prisma.order.create({
+            data: {
+                userId: result.id,
+                total: total,
+                orderDetails: {
+                    create: cart.map(c => {
+                        const orderDetail: OrderDetail = {
+                            productId: c.productId,
+                            quantity: c.quantity,
+                            unitPrice: c.price
+                        };
+                        return orderDetail;
+                    })
+                }
+            }
+        });
+        // update and insert using transaction
+        await prisma.$transaction([...updateProductsPromises, createOrderPromise]);
 
+        // return created order id and success message
         res.json({
             success: true,
-            msg: 'Order success! We will contact you to confirm the order.'
+            msg: 'Order success! We will contact you to confirm the order.',
+            orderId: (await createOrderPromise).id
         })
 
     } catch (err) {
+        // show error message if there is an error
         if (typeof err === "string") {
             return res.status(500).json({ success: false, msg: err });
         } else if (err instanceof Error) {
