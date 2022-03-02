cd219917bc25340378ef5c10b52ee5b3609db044
jonabutt <jonathanbuttigieg1@gmail.com>
2022-03-02 22:04:05 +0100

    feat: arranged return type for middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,49 +1,54 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import jwt from 'jsonwebtoken';
-import {PrismaClient} from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { JwtPayload } from '../interfaces';
 
 const prisma = new PrismaClient();
 
-export type ErrorResponseData = {
+export type MiddlewareErrorResponseData = {
     success: boolean,
     msg: string
 }
 
-export type SuccessResponseData = {
-    success: boolean,
+export type MiddlewareSuccessResponseData = {
     id: string,
     roleId: string,
     isRoot: boolean
 }
 
 const auth = async (
-        req: NextApiRequest,
-        res: NextApiResponse<ErrorResponseData | SuccessResponseData>
-    ) => {
-        const token = req.headers.authorization as string;
-        
-        if(token === null){
-            return res.status(400)
-                .json({ success: false, msg: 'Invalid Authentication'});
-        }
-        
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string) as JwtPayload;
-        if(decodedToken === null){
-            return res.status(400)
-                .json({ success: false, msg: 'Invalid Authentication'});
-        }
+    req: NextApiRequest,
+    res: NextApiResponse<MiddlewareErrorResponseData | MiddlewareSuccessResponseData>
+) => {
+    const token = req.headers.authorization as string;
 
-        const user = await prisma.user.findFirst({where: {
+    if (token === null) {
+        return res.status(400)
+            .json({ success: false, msg: 'Invalid Authentication' });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string) as JwtPayload;
+    if (decodedToken === null) {
+        return res.status(400)
+            .json({ success: false, msg: 'Invalid Authentication' });
+    }
+
+    const user = await prisma.user.findFirst({
+        where: {
             id: decodedToken.id
-        }});
-
-        if(user === null){
-            return res.status(400)
-                .json({ success: false, msg: 'Invalid Authentication'});
         }
+    });
 
-        return { id: user.id, role: user.roleId, isRoot: user.isRoot };
+    if (user === null) {
+        return res.status(400)
+            .json({ success: false, msg: 'Invalid Authentication' });
+    }
+    const middlewareReturn: MiddlewareSuccessResponseData = {
+        id: user.id,
+        roleId: user.roleId,
+        isRoot: user.isRoot
+    }
+    return middlewareReturn;
 }
 
 export default auth;
