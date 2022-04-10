ee963dab07503803576ac55bf586d9b236e65cc0
jonabutt <jonathanbuttigieg1@gmail.com>
2022-04-10 19:50:33 +0200

    feat: add new category

--- pages/api/categories/index.tsx
+++ pages/api/categories/index.tsx
@@ -1,13 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { CategoryDTO } from "../../../interfaces";
-import { MiddlewareErrorResponseData, MiddlewareSuccessResponseData } from "../../../middleware/auth";
+import auth, { MiddlewareErrorResponseData, MiddlewareSuccessResponseData } from "../../../middleware/auth";
 
 const prisma = new PrismaClient();
 
-type SuccessResponseData = {
+type SuccessGetCategoriesResponseData = {
     success: boolean,
     categories: CategoryDTO[]
+}
+
+type SuccessCreateCategoryResponseData = {
+    success: boolean,
+    newCategory: CategoryDTO
 }
 type ErrorResponseData = {
     success: boolean,
@@ -16,18 +21,64 @@ type ErrorResponseData = {
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<SuccessResponseData | ErrorResponseData>
+    res: NextApiResponse<MiddlewareErrorResponseData | MiddlewareSuccessResponseData | SuccessCreateCategoryResponseData|SuccessGetCategoriesResponseData | ErrorResponseData>
 ) {
     switch (req.method) {
+        case "POST":
+            await createCategory(req, res)
+            break;
         case "GET":
             await getCategories(req, res)
             break;
     }
 }
 
+const createCategory = async (
+    req: NextApiRequest,
+    res: NextApiResponse<MiddlewareErrorResponseData | MiddlewareSuccessResponseData | SuccessCreateCategoryResponseData | ErrorResponseData>
+) => {
+    try {
+         // check if the user is authorized
+         const result = await auth(req, res) as MiddlewareSuccessResponseData;
+         // check if user role is admin
+        if(result.roleId != "ckzqt1gxo0081psu7ap73rabs"){
+            return res.status(404).json({ success: false, msg: "Authentication is not valid" });
+        }
+        
+        const {name} = req.body;
+        // check name if it filled
+        if(!name){
+            return res.status(404).json({ success: false, msg: "Name can't be left empty." });
+        }
+        // create a new category using prisma
+        const newCategory = await prisma.category.create({
+            data:{
+                name:name
+            }
+        });
+
+        // return new category
+        return res.json({ 
+            success: true, 
+            newCategory: {
+                id: newCategory.id,
+                name: newCategory.name
+            }
+        });
+    }
+    catch (err) {
+        // show error message if there is an error
+        if (typeof err === "string") {
+            return res.status(500).json({ success: false, msg: err });
+        } else if (err instanceof Error) {
+            return res.status(500).json({ success: false, msg: (err as Error).message });
+        }
+    }
+}
+
 const getCategories = async (
     req: NextApiRequest,
-    res: NextApiResponse<SuccessResponseData | ErrorResponseData>
+    res: NextApiResponse<SuccessGetCategoriesResponseData | ErrorResponseData>
 ) => {
     try {
         // get categories from database
--- pages/categories.tsx
+++ pages/categories.tsx
@@ -1,56 +1,105 @@
-import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Box, Button, FormControl, InputLabel, OutlinedInput, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useContext } from 'react';
+import { useContext, useState,FormEvent} from 'react';
+import toast from 'react-hot-toast';
+import { ActionKind } from '../store/Actions';
 import { DataContext } from '../store/GlobalState';
+import { postData } from '../utils/fetchAPI';
+
 
 const Categories: NextPage = () => {
     const { state, dispatch } = useContext(DataContext);
     const { auth,categories } = state;
+    const [ name, setName ] = useState("");
+
+    const paperStyle = { padding:20, display: "flex", flexDirection:"column", alignItems: "center"};
+
     if (auth === null) return null;
+
+    const handleSubmit = async (event: (FormEvent<HTMLFormElement>)) => {
+        event.preventDefault();
+        // show loading backdrop
+        dispatch({ type: ActionKind.SET_LOADING, payload: true });
+        // post new category to api
+        const res = await postData(
+            "categories",
+            {name:name},
+            auth.token
+        );
+        // check api result if success
+        if(res.success === true){
+            // add new category to store
+            dispatch({type: ActionKind.ADD_CATEGORIES, payload: [...categories, res.newCategory]})
+            toast.success(res.msg);
+        }
+        else{
+            toast.error(res.msg);
+        }
+        // hide load screen backdrop
+        dispatch({ type: ActionKind.SET_LOADING, payload: false });
+    }
+   
     return <>
         <Head>
             <title>Categories</title>
         </Head>
         <h2>Categories</h2>
+        <Paper elevation={2} >
+            <form onSubmit={handleSubmit} style={paperStyle}>
+                <FormControl  sx={{ m: 1 }} fullWidth variant="outlined" required>
+                    <InputLabel htmlFor="categorydata-name">Name</InputLabel>
+                    <OutlinedInput 
+                        id="categorydata-name"
+                        onChange={ e => setName(e.currentTarget.value)} 
+                        value={name} 
+                        name="name"
+                        label="Name"
+                        placeholder='Enter Category Name'
+                    />
+                </FormControl>
+                <Button type="submit" color="primary" fullWidth>Create Category</Button>
+
+            </form>
+        </Paper>
         <Box mt={0.8}>
-                <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 650 }} aria-label="categories table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>
-                                    Id
-                                </TableCell>
-                                <TableCell>
-                                    Name
-                                </TableCell>
-                                <TableCell>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="categories table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                                Id
+                            </TableCell>
+                            <TableCell>
+                                Name
+                            </TableCell>
+                            <TableCell>
 
-                                </TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {
-                                categories.map(c => (
-                                    <TableRow key={c.id}>
-                                        <TableCell>
-                                            {c.id}
-                                        </TableCell>
-                                        <TableCell>
-                                            {c.name}
-                                        </TableCell>
-                                        <TableCell>
-                                           
-                                        </TableCell>
-                                    </TableRow>
-                                ))
-                            }
-                        </TableBody>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            categories.map(c => (
+                                <TableRow key={c.id}>
+                                    <TableCell>
+                                        {c.id}
+                                    </TableCell>
+                                    <TableCell>
+                                        {c.name}
+                                    </TableCell>
+                                    <TableCell>
+                                        
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                        }
+                    </TableBody>
 
-                    </Table>
-                </TableContainer>
+                </Table>
+            </TableContainer>
 
-            </Box>
+        </Box>
     </>
 }
 
