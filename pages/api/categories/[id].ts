ac2193eacc960e17698d0d04a145e35437a97362
jonabutt <jonathanbuttigieg1@gmail.com>
2022-04-15 17:32:56 +0200

    Revert "feat: update category"
    This reverts commit 222b2c1aff316887b8acb6a96d290a7c2c0ed031.

--- pages/api/categories/[id].ts
+++ pages/api/categories/[id].ts
@@ -5,87 +5,29 @@ import auth, { MiddlewareErrorResponseData, MiddlewareSuccessResponseData } from
 
 const prisma = new PrismaClient();
 
-// type SuccessResponseData = {
-//     success: boolean,
-//     newCategory: CategoryDTO
-// }
-type ResponseData = {
+type SuccessResponseData = {
     success: boolean,
-    msg: string
+    newCategory: CategoryDTO
 }
-
-type SuccessUpdateResponseData = {
+type ErrorResponseData = {
     success: boolean,
-    updatedCategory: CategoryDTO,
     msg: string
 }
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<MiddlewareErrorResponseData | MiddlewareSuccessResponseData | ResponseData>
+    res: NextApiResponse<MiddlewareErrorResponseData | MiddlewareSuccessResponseData | SuccessResponseData | ErrorResponseData>
 ) {
     switch (req.method) {
-        case "PUT":
-            await updateCategory(req, res)
-            break;
         case "DELETE":
             await deleteCategory(req, res)
             break;
     }
 }
 
-const updateCategory = async (
-    req: NextApiRequest,
-    res: NextApiResponse<MiddlewareErrorResponseData | MiddlewareSuccessResponseData | SuccessUpdateResponseData| ResponseData>
-) => {
-    try {
-         // check if the user is authorized
-         const result = await auth(req, res) as MiddlewareSuccessResponseData;
-
-         // check if user role is admin
-        if(result.roleId != "ckzqt1gxo0081psu7ap73rabs"){
-            return res.status(404).json({ success: false, msg: "Authentication is not valid" });
-        }
-        
-        const id = req.query.id as string;
-        const {name} = req.body;
-        // check name if it filled
-        if(!name){
-            return res.status(404).json({ success: false, msg: "Name can't be left empty." });
-        }
-        // delete category using id
-        await prisma.category.update({
-            where:{
-                id:id
-            },
-            data:{
-                name:name
-            }
-        });
-
-        // return success message
-        return res.json({ 
-            success: true,
-            updatedCategory:{
-                id,
-                name
-            },
-            msg: "Category was updated successfully!"
-        });
-    }
-    catch (err) {
-        // show error message if there is an error
-        if (typeof err === "string") {
-            return res.status(500).json({ success: false, msg: err });
-        } else if (err instanceof Error) {
-            return res.status(500).json({ success: false, msg: (err as Error).message });
-        }
-    }
-}
-
 const deleteCategory = async (
     req: NextApiRequest,
-    res: NextApiResponse<MiddlewareErrorResponseData | MiddlewareSuccessResponseData | ResponseData>
+    res: NextApiResponse<MiddlewareErrorResponseData | MiddlewareSuccessResponseData | SuccessResponseData | ErrorResponseData>
 ) => {
     try {
          // check if the user is authorized
