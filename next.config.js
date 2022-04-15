617d1b34ebce911356dbd9654dfb09fba3629a32
jonabutt <jonathanbuttigieg1@gmail.com>
2022-04-15 16:57:06 +0200

    fix: base url

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    "BASE_URL": "http://localhost:3000"
+    "BASE_URL": "https://fabulous-sable-461f36.netlify.app"
   },
   images:{
     domains: ['images.unsplash.com']
